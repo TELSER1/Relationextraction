36e570cfb119194a09be59fabaf4462c205486ea
vagrant <vagrant@precise64.(none)>
2014-01-25 23:41:35 +0000

    removed duplicate function, put gen_lexical_features back in

--- convenience_functions.py
+++ convenience_functions.py
@@ -10,21 +10,77 @@ def preprocess(document):
     sentences=[nltk.ne_chunk(sent) for sent in sentences]
     return(sentences)
 #determine if pair is found in sentence, only record those sentences(label propagation)
-def find_pair(pair,doc):
-    '''This function finds and returns sentences with a pair of words them'''
-    rs=[]
-    for i in doc:
-        word1=False
-        word2=False
-        for j in i:
-            if pair[0].lower() in j:
-                word1=True
-            if pair[1].lower() in j:
-                word2=True
-            if word1==True and word2==True:
-                rs.append(i)
-                break
-    return(rs)
+
+def gen_lexical_features(sentence,k=1,stem_and_lem=True):
+    '''This function computes lexical features on a word by word basis for future processing.  <k> specifies the window around a token to extract part of speech tags
+    and words from; stemming and lemmatizing options exist for the word token'''
+    featurelist=[]
+    f
+    s_len=len(sentence)
+    stemmer=nltk.stem.porter.PorterStemmer()
+    lemmatizer=nltk.stem.wordnet.WordNetLemmatizer()
+    #parse through sentence elements
+    for ix,tup in enumerate(sentence):
+        features={}
+        if not isinstance(sentence[ix],nltk.tree.Tree):
+            features['word']=sentence[ix][0]
+        else:
+            for tup in sentence[ix][0]:
+                word.append(tup[0])
+            features['word']=" ".join(word)
+                    
+            
+        #get words/POS tags in neighborhood around a word
+        for distance in range(1,k+1):
+            #make sure we don't hit indexing errors
+            if ix-distance<0:
+                features['pos'+'l'+str(distance)]='beginningofsentence'
+                features['word'+'l'+str(distance)]='beginningofsentence'
+            else:
+                #preprocessing leaves trees instead of tuples for named entities
+                if not isinstance(sentence[ix-distance],nltk.tree.Tree):
+                    features['pos'+'l'+str(distance)]=sentence[ix-distance][1]
+                    #checking for stem setting
+                    if stem_and_lem is False:
+                        features['word'+'l'+str(distance)]=sentence[ix-distance][0]
+                    else:
+                        features['word'+'l'+str(distance)]=stemmer.stem(lemmatizer.lemmatize(sentence[ix-distance][0]))
+                else:
+                    features['pos'+'l'+str(distance)]=sentence[ix-distance].node
+                    word=[]
+                    for tup in sentence[ix-distance][0]:
+                        word.append(tup[0])
+                    if stem_and_lem is False:
+                        features['word'+'l'+str(distance)]=" ".join(word)
+                    else:
+                        features['word'+'l'+str(distance)]=stemmer.stem(lemmatizer.lemmatize(" ".join(word)))                
+                    
+                    
+            if ix+distance>(s_len-1):
+                features['pos'+'r'+str(distance)]='endofsentence'
+                features['word'+'r'+str(distance)]='endofsentence'
+            else:
+                if not isinstance(sentence[ix+distance],nltk.tree.Tree):
+                    features['pos'+'r'+str(distance)]=sentence[ix+distance][1]
+                    #checking for stem setting
+                    if stem_and_lem is False:
+                        features['word'+'r'+str(distance)]=sentence[ix+distance][0]
+                    else:
+                        features['word'+'r'+str(distance)]=stemmer.stem(lemmatizer.lemmatize(sentence[ix+distance][0]))
+                else:
+                    features['pos'+'r'+str(distance)]=sentence[ix+distance].node
+                    word=[]
+                    for tup in sentence[ix+distance][0]:
+                        word.append(tup[0])
+                    if stem_and_lem is False:
+                        features['word'+'r'+str(distance)]=" ".join(word)
+                    else:
+                        features['word'+'r'+str(distance)]=stemmer.stem(lemmatizer.lemmatize(" ".join(word)))
+        
+        featurelist.append(features)
+    features['pair']    
+    return(featurelist)
+
 
 #determine if pair is found in sentence, only record those sentences(label propagation)
 def find_pair(pair,doc):
